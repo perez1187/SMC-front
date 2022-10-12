3a6e89297ecf37392d6f230762eaf10460644a7a
perez1187 <o.perez1187@gmail.com>
2022-10-12 19:40:19 +0200

    reset pas

--- SMC-front/src/services/user-services.jsx
+++ SMC-front/src/services/user-services.jsx
@@ -44,3 +44,18 @@ export function resendingActivationEmail(userData) {
         console.log(e)
     })
 }
+
+//forgot password
+export function requestPasswordResetEmail(userData) {
+    return fetch('http://127.0.0.1:8000/auth/request-reset-email/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }
+    ).then(resp => resp.json())
+    .catch( e => {
+        console.log(e)
+    })
+}
