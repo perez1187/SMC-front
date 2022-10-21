99d7ff11c3d6a7b057feb313a1086f492f794386
perez1187 <o.perez1187@gmail.com>
2022-10-21 17:22:27 +0200

    conecting instructor card 1

--- SMC-front/src/components/InstructorsComponent/InstructorsComponent.jsx
+++ SMC-front/src/components/InstructorsComponent/InstructorsComponent.jsx
@@ -44,56 +44,28 @@ function InstructorsComponent() {
                     <div key={element.id}  className="InstructorProfileBox" >                        
                         <div className='InstructorProfileBoxPersonalData'> {/* Avatar, name, title, game, opinions */}
                             
-                            <div className='InstructorProfileBoxAvatarCountr'> {/* avatar and country flag */}
-                                <div className='InstructorProfileBoxAvatar'></div>
-                                {/* <div className='InstructorProfileBoxCountryFlag'></div> */}
+                            <div > {/* avatar and country flag */}
+                                <div className='InstructorProfileBoxAvatar' style={{
+                                    backgroundImage: `url(${element.avatar})`,
+                                    width:"200px",
+                                    height:'200px',
+                                    backgroundRepeat:"no-repeat",
+                                    backgroundSize:"cover" 
+                                    }} ></div>
+                                <div className='InstructorProfileBoxCountryFlag'  style={{
+                                    backgroundImage: `url('https://flagcdn.com/40x30/pl.png')`,
+                                    backgroundRepeat:"no-repeat",
+                                    backgroundSize:"cover" 
+                                    }} ></div>
                                 <div className='InstructorProfileBoxTypeOfGame'> Chess </div> {/* type of game */}
                             </div>{/* avatar and country flag */}
 
-                            <div className='InstructorProfileBoxBoxName'> {/* title, name, game, opinions */}
+      
 
-                                <div className='InstructorProfileBoxTitleName'> {/* title name */}
-                                    <div className='InstructorProfileBoxTitle'>GM</div> 
-                                    <div className='InstructorProfileBoxNameName'> Kaczmarek Piotr</div>                             
-                                </div> {/* className='InstructorProfileBoxTitleName' */}    
-
-                                <div className='InstructorProfileBoxCountryFlag'></div>
-
-                                <div className='InstructorProfileBoxELORating'> ELO rating: </div> {/* type of game */}
-                                <div className='InstructorProfileBoxRatingActualRating'> 
-                                    <div className='InstructorProfileBoxRatingActualRatingActual'>Actual:</div>
-                                    <div className='InstructorProfileBoxRatingActualRatingRating'>1900</div>
-                                    <div className='InstructorProfileBoxRatingActualRatingType'>standart</div>                                 
-                                </div> {/* actual rating */}
-                                <div className='InstructorProfileBoxRatingPB'> 
-                                    <div  className='InstructorProfileBoxRatingActualRatingActual'> Personal Best:</div>
-                                    <div className='InstructorProfileBoxRatingPBRating'>2063</div>
-                                    {/* <div> 01/01/2005</div> */}
-                                    <div className='InstructorProfileBoxRatingActualRatingType'>standart</div>
-                                </div> {/* type of game */}
-
-                                <div className='InstructorProfileBoxOpinions'> </div> {/* opinions */}
-                            </div> {/*InstructorProfileBoxBoxName  */}
 
                         </div>{/* InstructorProfileBoxPersonalData */}
-                        <div className='InstructorProfileBoxSMBox'>
-                            <img className='InstructorProfileBoxSMBoxIcon' src={InstagramIcon} alt = "FBIcon" style={{ display: "display"}} />
-                            <img className='InstructorProfileBoxSMBoxIcon' src={LichessIcon} alt = "FBIcon" style={{display: "display"}} />
-                            <img className='InstructorProfileBoxSMBoxIcon' src={FbICon} alt = "FBIcon" style={{display: "display"}} />
-                            <img className='InstructorProfileBoxSMBoxIcon' src={LinkedinIcon} alt = "FBIcon" style={{ display: "display"}} />
-                            <img className='InstructorProfileBoxSMBoxIcon' src={ChesscomIcon} alt = "FBIcon" style={{display: "display"}} />
-                            <img className='InstructorProfileBoxSMBoxIcon' src={TwitterIcon} alt = "FBIcon" style={{display: "display"}} />
-                            <img className='InstructorProfileBoxSMBoxIcon' src={EmailIcon} alt = "FBIcon" style={{display: "display"}} />
-                        </div>
-
-                        <div style={{display: "none"}}> my link</div>
-                        <div> cena za konsultacje 100 pln /h</div>
-                        <div> treningi online: tak treningi na żywo: nie</div>
-                        <div> jezyki; polski angielski</div>
-                        <div> miasto: Poznan kraj: Polska</div>
-                        <div> Filmy onilne: Tak</div>
-                        <div>opis: Lorem ipsumm</div>
-                                  {/*  */}
+   
+ 
                     </div>
                 )
             }
