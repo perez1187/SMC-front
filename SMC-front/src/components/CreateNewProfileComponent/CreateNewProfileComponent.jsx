074faac8af3ebc7b652201268b6060a9de5d19f8
perez1187 <o.perez1187@gmail.com>
2022-11-04 19:12:36 +0100

    working on create profile

--- SMC-front/src/components/CreateNewProfileComponent/CreateNewProfileComponent.jsx
+++ SMC-front/src/components/CreateNewProfileComponent/CreateNewProfileComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // using context
 import {useAuth} from '../../hooks/useAuth'
@@ -7,15 +7,20 @@ import {useAuth} from '../../hooks/useAuth'
 import { CreateNewProfile } from '../../services/profile-services'
 import { refreshAccessToken } from '../../services/user-services'
 
+// css
+import './CreateNewProfileComponent.css'
+
 function CreateNewProfileComponent() {
 
     // useStates for context
     const {authData,setAuth} =useAuth()
-    // console.log(authData.id)
 
+    // useStates for form
+    const [name, setName] = useState('')
+    const [lastName, setLastName] = useState('')
 
-
-
+    console.log(name)
+ 
     // function for xreate new profile
     const handleSubmit = async e => {
         //e.preventDefault() // we are not going to refresh the page
@@ -37,56 +42,140 @@ function CreateNewProfileComponent() {
                     const data = await CreateNewProfile( newAccessToken.access, {'user':authData.id}) 
                     console.log(data)
                 }
-
             }
-
         else {
             console.log("you need to login")
             // we need to send info to user that he needs to login
         }
-
-
-    
-    // 👇️ navigate to /
-    // const navigateHome = async () => {           
-    //     navigate('/');          
-    //             };
-
-
-    // we get back email and token from api in data
-    // and if we have data.emai (login succes) we set as context and move to landing page
-    // varables for loggind
-    // try {
-    //     if (data.email) {
-    //         if (data.email == 'This field may not be blank.') {
-    //             // console.log("chujjjj nie pusty")
-    //         } else if (data.password == 'This field may not be blank.'){
-                
-    //         }
-    //         else if (data.email == 'Enter a valid email address.'){
-                
-    //         }
-    //         else {
-    //             await setAuth(data)
-    //             await navigateHome() 
-    //         }
-
-    //     }
-
-    //     } catch (e) {
-    //     console.log('Error')
-        
-    //     // tutaj dac use state
-    //     }
-    //     setLoginNotSuccess(true)           
-    
     }
-
 
   return (
     <div>
         CreateNewProfileComponent
-        <button onClick={()=>handleSubmit()}> create</button>
+        <div>
+            <button>Chess Instructor</button>
+            <button>Draughts Instructor</button>
+            <button>Academy/Club</button>
+        </div>
+        
+        {/* form for creating profile */}
+        <form onSubmit={handleSubmit} className='CNPForm'>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> First Name:</div>
+                <input type="text" onChange={ e => setName(e.target.value)} />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Last Name:</div>
+                <input type="text" />
+            </label >
+            <label className='CNPLabel'>
+                Avatar:
+                {/* <input type="text" onChange={ e => setName(e.target.value)} /> */}
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Country:</div>
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Chess Titile:</div>
+                <input type="text" />
+            </label>
+            Rating (fide.com)
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Actual Rating Std</div>
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Actual Rating Rapid:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Actual Rating Blitz:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Top Rating Std:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Top Rating Std Year:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Top Rating Rapid:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Top Rating Rapid Year:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Top Rating Blitz:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Top Rating Blitz Year:</div>                
+                <input type="text" />
+            </label>
+            Socials
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Facebook</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Instagram:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Twitter:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Youtube:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Tiktok</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Fide.com:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Chess.com:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Lichess.org:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Languages:</div>                
+                <input type="text" />
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Description:</div>                
+                <textarea rows='5' cols="50" value=''>description </textarea>
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Successes:</div>                
+                <textarea rows='5' cols="50" value=''> </textarea>
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Teaching Experience:</div>                
+                <textarea rows='5' cols="50" value=''> </textarea>
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> About Me:</div>                
+                <textarea rows='5' cols="50" value=''> </textarea>
+            </label>
+            <label className='CNPLabel'>
+                <div className='CNPLabelName'> Message to SMC (optional):</div>                
+                <textarea rows='5' cols="50" value=''> </textarea>
+            </label>
+        </form>
+        {/* <button onClick={()=>handleSubmit()}> create</button> */}
     </div>
   )
 }
