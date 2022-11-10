be52284ee08bd729c5e7452d9f169ebd0d62606f
perez1187 <o.perez1187@gmail.com>
2022-11-10 12:33:22 +0100

    create instructor profile, JSON fields for chess/checkesrs add info

--- SMC-front/src/components/CreateNewProfileComponent/CreateNewProfileComponent.jsx
+++ SMC-front/src/components/CreateNewProfileComponent/CreateNewProfileComponent.jsx
@@ -22,6 +22,8 @@ function CreateNewProfileComponent() {
     const [slug,setSlug] = useState('') // profilename
     const [avatar, setAvatar] = useState()
     const [country, setCountry] = useState('')
+
+    // chess
     const [chessTitile, setChessTitle] = useState('')
     const [actualRatingStdChess, setActualRatingStdChess] = useState('')
     const [actualRatingRapidChess, setActualRatingRapidChess] = useState('')
@@ -33,18 +35,43 @@ function CreateNewProfileComponent() {
     const [topRatingBlitzChess, setTopRatingBlitzChess] = useState('')
     const [topRatingBlitzChessYear, setTopRatingBlitzChessYear] = useState('')
 
+    // checkers
     const [checkersTitile, setCheckersTitle] = useState('')
     const [actualRatingStdCheckers, setActualRatingStdCheckers] = useState('')
     const [actualRatingBlitzCheckers, setActualRatingBlitzCheckers] = useState('')
     const [topRatingStdCheckers, setTopRatingStdCheckers] = useState('')
     const [topRatingStdCheckersYear, setTopRatingStdCheckersYear] = useState('')
     const [topRatingBlitzCheckers, setTopRatingBlitzCheckers] = useState('')
-    const [topRatingBlitzChessCheckers, setTopRatingBlitzChessCheckers] = useState('')
+    const [topRatingBlitzCheckersYear, setTopRatingBlitzCheckersYear] = useState('')
 
+    // social media
+    const [FB, setFB] = useState('')
+    const [instagram, setInstagram] = useState('')
+    const [twitter, setTwitter] = useState('')
+    const [yt, setYt] = useState('')
+    const [tiktok, setTiktok] = useState('')
 
-    console.log(actualRatingStdChess)
- 
-    // function for create new profile
+    // social chess
+    const [fide, setFide] = useState('')
+    const [chess_com, setChess_com] = useState('')
+    const [lichess, setLichess] = useState('')
+
+    // social checkets
+    const [fmjd, setFmjd] = useState('')
+
+    // other for later
+    const [languages, setLanguages] = useState('')
+
+    // profile description
+    const [description, setDescription] = useState('')
+    const [successes, setSuccesses] = useState('')
+    const [experience, setexperience] = useState('')
+    // const [aboutMe, setAboutMe] = useState('')
+    const [messageToSMC, setMessageToSMC] = useState('')
+    
+    console.log(actualRatingBlitzChess)
+
+     // function for create new profile
     const handleSubmit = async e => {
         // e.preventDefault() // we are not going to refresh the page
         // console.log("wszedlem")
@@ -66,10 +93,32 @@ function CreateNewProfileComponent() {
  
                     // form data automatically create  'Content-Type'
                     const createData = new FormData()
+
                     const defaultChessProfile = {
-                        "chess_title":"GM",
-                        "top_rating":"2350",
-                        "actual_rating":actualRatingStdChess
+                        "chess_title":"GM",                        
+                        "actual_rating":actualRatingStdChess,
+                        "actual_ratingRapid":actualRatingRapidChess,
+                        "actual_ratingBlitz":actualRatingBlitzChess,
+                        "top_rating":topRatingStdChess,
+                        "top_rating_date":topRatingStdChessYear,
+                        "top_ratingRapid":topRatingRapidChess,
+                        "top_ratingRapid_date":topRatingRapidChessYear,
+                        "top_ratingBlitz":topRatingBlitzChess,
+                        "top_ratingBlitz_date":topRatingBlitzChessYear,                                                 
+                        "chess.com":chess_com,
+                        "fide.com":fide,
+                        "lichess.org":lichess,                        
+                    }
+
+                    const defaultCheckersProfile = {
+                        "draughts_title":"GM",                        
+                        "actual_rating":actualRatingStdCheckers,
+                        "actual_ratingBlitz":actualRatingBlitzCheckers,
+                        "top_rating":topRatingStdCheckers,
+                        "top_rating_date":topRatingStdCheckersYear,
+                        "top_ratingBlitz":topRatingBlitzCheckers,
+                        "top_ratingBlitz_date":topRatingBlitzCheckersYear,                                                 
+                        "fmjd.org":fmjd,                      
                     }
 
                     // we add all fields to FormData like that:
@@ -77,6 +126,7 @@ function CreateNewProfileComponent() {
                     createData.append('avatar',avatar)
                     createData.append('slug',slug)
                     createData.append('chess_profile',JSON.stringify(defaultChessProfile))
+                    createData.append('checkers_profile',JSON.stringify(defaultCheckersProfile))
 
                     // we call CreateNewProfile function and paste token + data
                     const data = await CreateNewProfile( 
@@ -93,7 +143,7 @@ function CreateNewProfileComponent() {
     }
 
   return (
-    <div>
+    <div className='CNPContainer'>
         CreateNewProfileComponent
         <div>
             <button>Chess Instructor</button>
@@ -104,17 +154,17 @@ function CreateNewProfileComponent() {
         
         {/* form for creating profile */}
         <form  className='CNPForm'>
-            <label className='CNPLabel'>
+            {/* <label className='CNPLabel'>
                 <div className='CNPLabelName'> Profile Visible (Landing Page):</div>
                 <input type="radio" />
-            </label >
+            </label > */}
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> First Name:</div>
-                <input type="text" onChange={ e => setName(e.target.value)} value='name'/>
+                <input type="text" onChange={ e => setName(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Last Name:</div>
-                <input type="text" />
+                <input type="text" onChange={ e => setLastName(e.target.value)}/>
             </label >
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Profile name </div>
@@ -140,35 +190,35 @@ function CreateNewProfileComponent() {
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Actual Rating Rapid:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setActualRatingRapidChess(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Actual Rating Blitz:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setActualRatingBlitzChess(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> The highest ranking Std:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingStdChess(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Year:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingStdChessYear(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> The highest ranking Rapid:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingRapidChess(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Year:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingRapidChessYear(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> The highest ranking Blitz:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingBlitzChess(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Year:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingBlitzChessYear(e.target.value)}/>
             </label>
             
             {/* checkers rating  */}
@@ -179,89 +229,89 @@ function CreateNewProfileComponent() {
             Rating (fmjd.org)
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Actual Rating Classic</div>
-                <input type="text" />
+                <input type="text" onChange={ e => setActualRatingStdCheckers(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Actual Rating Blitz:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setActualRatingBlitzCheckers(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Top Rating Classic:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingStdCheckers(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Top Rating Classic Year:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingStdCheckersYear(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Top Rating Blitz:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingBlitzCheckers(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Top Rating Blitz Year:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTopRatingBlitzCheckersYear(e.target.value)}/>
             </label>
 
             Socials
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Facebook</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setFB(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Instagram:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setInstagram(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Twitter:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTwitter(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Youtube:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setYt(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Tiktok</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setTiktok(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Fide.com:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setFide(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Chess.com:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setChess_com(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Lichess.org:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setLichess(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> fmjd.org:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setFmjd(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Languages:</div>                
-                <input type="text" />
+                <input type="text" onChange={ e => setLanguages(e.target.value)}/>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Description:</div>                
-                <textarea rows='5' cols="50" value=''>description </textarea>
+                <textarea rows='5' cols="50" value='' onChange={ e => setDescription(e.target.value)}>description </textarea>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Successes:</div>                
-                <textarea rows='5' cols="50" value=''> </textarea>
+                <textarea rows='5' cols="50" value=''onChange={ e => setSuccesses(e.target.value)}> </textarea>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Teaching Experience:</div>                
-                <textarea rows='5' cols="50" value=''> </textarea>
+                <textarea rows='5' cols="50" value=''onChange={ e => setexperience(e.target.value)}> </textarea>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> About Me:</div>                
-                <textarea rows='5' cols="50" value=''> </textarea>
+                <textarea rows='5' cols="50" value=''onChange={ e => setAboutMe(e.target.value)}> </textarea>
             </label>
             <label className='CNPLabel'>
                 <div className='CNPLabelName'> Message to SMC (optional):</div>                
-                <textarea rows='5' cols="50" value=''> </textarea>
+                <textarea rows='5' cols="50" value=''onChange={ e => setMessageToSMC(e.target.value)}> </textarea>
             </label>
                            
             
